27ad5134f48e69bcb5405b2df5eea153aa53a105
zhouyingkai <zhouyingkai@timeface.cn>
2016-09-05 15:52:51 +0800

    添加首页

--- js/public.js
+++ js/public.js
@@ -1,6 +1,6 @@
 var timer=null;
-var bsym=true;
-backTop.onclick=function(){
+var bsym=true;   //判断是用户拖动滚动条 还是 返回顶部按钮使滚动条滚动
+backTop.onclick=function(){  //返回顶部
   timer=setInterval(function(){
     var scrollTop=document.documentElement.scrollTop||document.body.scrollTop;
     if (scrollTop==0)
@@ -12,7 +12,7 @@ backTop.onclick=function(){
   },30)
 };
 
-window.addEventListener('scroll',function(){
+window.addEventListener('scroll',function(){ //页面滚动悬浮导航显示隐藏
   var scrollTop=document.body.scrollTop;
   if(scrollTop>=160){
 
@@ -32,4 +32,13 @@ window.addEventListener('scroll',function(){
     $('.back').fadeOut();
   }
 });
-
+window.addEventListener('load',function(){  //联系我们交谈框显示隐藏
+  $('.concat_btn').click(function(){
+    $(this).fadeOut();
+    $('.concat').fadeIn();
+  });
+  $('.close').click(function(){
+    $('.concat').fadeOut();
+    $('.concat_btn').fadeIn();
+  })
+});
