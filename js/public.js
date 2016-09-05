78ff3e7132669e917d9642994b0f1b101c5505c2
zhouyingkai <zhouyingkai@timeface.cn>
2016-09-05 14:36:23 +0800

    添加首页

--- js/public.js
+++ js/public.js
@@ -1,11 +1,35 @@
+var timer=null;
+var bsym=true;
+backTop.onclick=function(){
+  timer=setInterval(function(){
+    var scrollTop=document.documentElement.scrollTop||document.body.scrollTop;
+    if (scrollTop==0)
+    {
+      clearInterval(timer);
+    }iSpeed=Math.floor(-scrollTop/8);
+    bsym=true;
+    document.documentElement.scrollTop=document.body.scrollTop=scrollTop+iSpeed;
+  },30)
+};
+
 window.addEventListener('scroll',function(){
   var scrollTop=document.body.scrollTop;
   if(scrollTop>=160){
 
     $('.top_nav').css('opacity','1').slideDown(300);
-    console.log(1);
   }else{
     $('.top_nav').slideUp(10);
-    console.log(2);
+  }
+
+  if (!bsym)
+  {
+    clearInterval(timer);
+  }
+  bsym=false;
+  if(scrollTop>=600){
+    $('.back').fadeIn();
+  }else{
+    $('.back').fadeOut();
   }
 });
+
